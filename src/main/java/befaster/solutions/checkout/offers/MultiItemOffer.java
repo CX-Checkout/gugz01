caea0d9a38b98fbcaa4106a6706bd3e598aa7fa5
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 20:18:36 +0000

    Mon Dec 04 20:18:36 GMT 2017

--- src/main/java/befaster/solutions/checkout/offers/MultiItemOffer.java
+++ src/main/java/befaster/solutions/checkout/offers/MultiItemOffer.java
@@ -1,4 +1,28 @@
 package befaster.solutions.checkout.offers;
 
+import befaster.solutions.checkout.SKUs;
+
 public class MultiItemOffer {
+
+    private final String sku;
+    private final int numberOfItems;
+    private final int discount;
+
+    public MultiItemOffer(String sku, int numberOfItems, int discount) {
+        this.sku = sku;
+        this.numberOfItems = numberOfItems;
+        this.discount = discount;
+    }
+
+    public DiscountResult discountFor(String skus) {
+        long quantityOfAs = SKUs.skuQuantity(skus, sku);
+        int numberOfTripleAs = (int) quantityOfAs / 3;
+        if (numberOfTripleAs > 0) {
+            String orderedSkus = SKUs.sort(skus);
+            String remainingSkus = orderedSkus.replaceAll("AAA", "");
+            int discount = numberOfTripleAs * 20;
+            return new DiscountResult(discount, remainingSkus);
+        }
+        return new DiscountResult(0, skus);
+    }
 }
--- src/test/java/befaster/solutions/checkout/offers/MultiItemOfferShould.java
+++ src/test/java/befaster/solutions/checkout/offers/MultiItemOfferShould.java
@@ -1,5 +1,33 @@
-import static org.junit.Assert.*;
+package befaster.solutions.checkout.offers;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
 public class MultiItemOfferShould {
+
+    @Test
+    @Parameters({
+            "A, 3, 20, AAA, 20, "
+    })
+    public void
+    return_discount(String sku,
+                    int numberOfItems,
+                    int discount,
+                    String inputSkus,
+                    int totalDiscount,
+                    String remainingSkus) {
+        MultiItemOffer multiItemOffer = new MultiItemOffer(sku, numberOfItems, discount);
+
+        DiscountResult discountResult = multiItemOffer.discountFor(inputSkus);
+
+        assertThat(discountResult.discount, is(totalDiscount));
+        assertThat(discountResult.remainingSkus, is(remainingSkus));
+    }
 
 }
