14cc76fe2ed6926105d5fea9c7227b8fc9f3d890
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 23:30:27 +0000

    Mon Dec 04 23:30:27 GMT 2017

--- src/main/java/befaster/solutions/checkout/offers/TwoEsOffer.java
+++ src/main/java/befaster/solutions/checkout/offers/TwoEsOffer.java
@@ -1,23 +1,27 @@
 package befaster.solutions.checkout.offers;
 
-import befaster.solutions.checkout.SKUs;
+import befaster.solutions.checkout.offers.combined_item.CombinedItemOffer;
 
-public class TwoEsOffer {
+public class TwoEsOffer extends CombinedItemOffer {
 
-    public static DiscountResult discountFor(String skus) {
-        long quantityOfAs = SKUs.skuQuantity(skus, "E");
-        int numberOfDoubleEs = (int) quantityOfAs / 2;
-        if (numberOfDoubleEs > 0) {
-            String remainingSkus = SKUs.sort(skus).replaceAll("EE", "");
-            int discount = 0;
-            for (int i = 0; i < numberOfDoubleEs; i++) {
-                if (remainingSkus.contains("B")) {
-                    discount += 30;
-                    remainingSkus = remainingSkus.replaceFirst("B", "");
-                }
-            }
-            return new DiscountResult(discount, remainingSkus);
-        }
-        return new DiscountResult(0, skus);
+    public TwoEsOffer() {
+        super("E", 2, 30, "B");
     }
+
+//    public static DiscountResult discountFor(String skus) {
+//        long quantityOfAs = SKUs.skuQuantity(skus, "E");
+//        int numberOfDoubleEs = (int) quantityOfAs / 2;
+//        if (numberOfDoubleEs > 0) {
+//            String remainingSkus = SKUs.sort(skus).replaceAll("EE", "");
+//            int discount = 0;
+//            for (int i = 0; i < numberOfDoubleEs; i++) {
+//                if (remainingSkus.contains("B")) {
+//                    discount += 30;
+//                    remainingSkus = remainingSkus.replaceFirst("B", "");
+//                }
+//            }
+//            return new DiscountResult(discount, remainingSkus);
+//        }
+//        return new DiscountResult(0, skus);
+//    }
 }
--- src/main/java/befaster/solutions/checkout/offers/combined_item/CombinedItemOffer.java
+++ src/main/java/befaster/solutions/checkout/offers/combined_item/CombinedItemOffer.java
@@ -1,4 +1,40 @@
 package befaster.solutions.checkout.offers.combined_item;
 
+import befaster.solutions.checkout.SKUs;
+import befaster.solutions.checkout.offers.DiscountResult;
+
 public class CombinedItemOffer {
+
+    private final String sku;
+    private final int numberOfItems;
+    private final int discount;
+    private final String freeSku;
+
+    public CombinedItemOffer(String sku,
+                             int numberOfItems,
+                             int discount,
+                             String freeSku) {
+
+        this.sku = sku;
+        this.numberOfItems = numberOfItems;
+        this.discount = discount;
+        this.freeSku = freeSku;
+    }
+
+    public DiscountResult discountFor(String skus) {
+        long quantityOfAs = SKUs.skuQuantity(skus, "E");
+        int numberOfDoubleEs = (int) quantityOfAs / 2;
+        if (numberOfDoubleEs > 0) {
+            String remainingSkus = SKUs.sort(skus).replaceAll("EE", "");
+            int discount = 0;
+            for (int i = 0; i < numberOfDoubleEs; i++) {
+                if (remainingSkus.contains("B")) {
+                    discount += 30;
+                    remainingSkus = remainingSkus.replaceFirst("B", "");
+                }
+            }
+            return new DiscountResult(discount, remainingSkus);
+        }
+        return new DiscountResult(0, skus);
+    }
 }
