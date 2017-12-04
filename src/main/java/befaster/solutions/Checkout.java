39eade996a63c57da502947a0b150f6071da3ff3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 08:38:26 +0000

    Mon Dec 04 08:38:26 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -6,47 +6,19 @@ import static com.google.common.base.Strings.isNullOrEmpty;
 
 public class Checkout {
 
-    static final HashMap<String, Integer> SKU_PRICE = createSKUIndividualPrices();;
-    static final String SKUS = allSKUs();
-
     public static Integer checkout(String skus) {
         if (isNullOrEmpty(skus)) return 0;
-        if (!valid(skus)) return -1;
+        if (!SKUs.valid(skus)) return -1;
         HashMap<String, Integer> skusQuantity = skusQuantityFor(skus);
         return priceWithoutDiscounts(skus) - discounts(skusQuantity);
-    }
-
-    private static HashMap<String, Integer> createSKUIndividualPrices() {
-        HashMap<String, Integer> skuPrices = new HashMap<>();
-        skuPrices.put("A", 50);
-        skuPrices.put("B", 30);
-        skuPrices.put("C", 20);
-        skuPrices.put("D", 15);
-        skuPrices.put("E", 40);
-        return skuPrices;
     }
 
     private static Integer priceWithoutDiscounts(String skus) {
         Integer totalPrice = 0;
         for (String sku : skus.split("")) {
-            totalPrice += SKU_PRICE.getOrDefault(sku, 0);
+            totalPrice += SKUs.priceFor(sku);
         }
         return totalPrice;
-    }
-
-    private static boolean valid(String skus) {
-        for (String sku : skus.split("")) {
-            if (!SKUS.contains(sku)) return false;
-        }
-        return true;
-    }
-
-    private static String allSKUs() {
-        String skus = "";
-        for (String sku : SKU_PRICE.keySet()) {
-            skus += sku;
-        }
-        return skus;
     }
 
     private static Integer discounts(HashMap<String, Integer> skusQuantity) {
--- src/main/java/befaster/solutions/SKUs.java
+++ src/main/java/befaster/solutions/SKUs.java
@@ -1,4 +1,39 @@
 package befaster.solutions;
 
+import java.util.HashMap;
+
 public class SKUs {
+
+    static final HashMap<String, Integer> SKU_PRICE = createSKUIndividualPrices();;
+    static final String SKUS = allSKUs();
+
+    private static HashMap<String, Integer> createSKUIndividualPrices() {
+        HashMap<String, Integer> skuPrices = new HashMap<>();
+        skuPrices.put("A", 50);
+        skuPrices.put("B", 30);
+        skuPrices.put("C", 20);
+        skuPrices.put("D", 15);
+        skuPrices.put("E", 40);
+        return skuPrices;
+    }
+
+    private static String allSKUs() {
+        String skus = "";
+        for (String sku : SKU_PRICE.keySet()) {
+            skus += sku;
+        }
+        return skus;
+    }
+
+    static boolean valid(String skus) {
+        for (String sku : skus.split("")) {
+            if (!SKUS.contains(sku)) return false;
+        }
+        return true;
+    }
+
+    static int priceFor(String sku) {
+        return SKU_PRICE.getOrDefault(sku, 0);
+    }
+
 }
