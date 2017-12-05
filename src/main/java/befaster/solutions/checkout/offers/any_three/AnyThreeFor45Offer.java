3e7cc924ba634b1058d6db11d2d64da8f1596673
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 00:55:38 +0000

    Tue Dec 05 00:55:38 GMT 2017

---
+++ src/main/java/befaster/solutions/checkout/offers/any_three/AnyThreeFor45Offer.java
@@ -0,0 +1,11 @@
+package befaster.solutions.checkout.offers.any_three;
+
+import befaster.solutions.checkout.offers.DiscountResult;
+import befaster.solutions.checkout.offers.Offer;
+
+public class AnyThreeFor45Offer implements Offer {
+    @Override
+    public DiscountResult discountFor(String skus) {
+        return null;
+    }
+}
--- src/test/java/befaster/solutions/checkout/offers/any_three/AnyThreeFor45OfferShould.java
+++ src/test/java/befaster/solutions/checkout/offers/any_three/AnyThreeFor45OfferShould.java
@@ -1,4 +1,32 @@
 package befaster.solutions.checkout.offers.any_three;
 
-public class AnyThreeFor45Offer {
+import befaster.solutions.checkout.offers.Offer;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class AnyThreeFor45OfferShould {
+
+    private Offer anyThreeFor45Offer;
+
+    @Before
+    public void initialise() {
+        anyThreeFor45Offer = new AnyThreeFor45Offer();
+    }
+
+    @Test
+    @Parameters({
+            "STX, 45"
+    })
+    public void
+    return_45_if_any_3_of_S_T_X_Y_Z(String skus, int discount) {
+        assertThat(anyThreeFor45Offer.discountFor(skus), is(discount));
+    }
+
 }
