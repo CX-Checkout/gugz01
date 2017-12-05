1eb89aa5a96ea159069825fdd3fdc3db0e044269
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 00:31:38 +0000

    Tue Dec 05 00:31:38 GMT 2017

--- src/main/java/befaster/solutions/checkout/offers/multi_item/TwoKsOffer.java
+++ src/main/java/befaster/solutions/checkout/offers/multi_item/TwoKsOffer.java
@@ -2,6 +2,6 @@ package befaster.solutions.checkout.offers.multi_item;
 
 public class TwoKsOffer extends MultiItemOffer {
     public TwoKsOffer() {
-        super("K", 2, 10);
+        super("K", 2, 20);
     }
 }
