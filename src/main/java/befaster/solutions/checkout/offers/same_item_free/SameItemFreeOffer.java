dd968c94ceff6508e6708bd9618d3a6d32034c6c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 08:47:36 +0000

    Tue Dec 05 08:47:36 GMT 2017

--- src/main/java/befaster/solutions/checkout/offers/same_item_free/SameItemFreeOffer.java
+++ src/main/java/befaster/solutions/checkout/offers/same_item_free/SameItemFreeOffer.java
@@ -1,11 +1,9 @@
 package befaster.solutions.checkout.offers.same_item_free;
 
-import befaster.solutions.checkout.SKUs;
 import befaster.solutions.checkout.offers.DiscountResult;
 import befaster.solutions.checkout.offers.Offer;
 
-import static befaster.solutions.checkout.SKUs.priceFor;
-import static befaster.solutions.checkout.SKUs.skuQuantity;
+import static befaster.solutions.checkout.SKUs.*;
 import static org.apache.commons.lang3.StringUtils.repeat;
 
 public class SameItemFreeOffer implements Offer {
@@ -20,13 +18,15 @@ public class SameItemFreeOffer implements Offer {
 
     @Override
     public DiscountResult discountFor(String skus) {
-        int numberOfTripleFs = skuQuantity(skus, sku) / numberOfItems;
-        String remainingSkus = "";
-        if (numberOfTripleFs > 0) {
-            String orderedSkus = SKUs.sort(skus);
-            remainingSkus = orderedSkus.replaceAll(repeat(sku, numberOfItems), "");
-            int totalDiscount = numberOfTripleFs * priceFor(sku);
+        int numberOfOffers = skuQuantity(skus, sku) / numberOfItems;
+        if (numberOfOffers > 0) {
+            String remainingSkus = removeSKUsPartOfTheOffer(skus);
+            int totalDiscount = numberOfOffers * priceFor(sku);
             return new DiscountResult(totalDiscount, remainingSkus);
         }
         return new DiscountResult(0, skus);    }
+
+    private String removeSKUsPartOfTheOffer(String skus) {
+        return sort(skus).replaceAll(repeat(sku, numberOfItems), "");
+    }
 }
